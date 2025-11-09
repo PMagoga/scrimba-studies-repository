8ba0ac0704c9b2c78a3466017a4d9a8270779ec7
PMagoga <pmagoga@gmail.com>
2025-11-09 20:52:36 -0300

    starting project two

---
+++ project_two/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Entry />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ project_two/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img src="../../src/assets/images/globe.png" alt="imagem do globo" />
+      <h1>My travel Journal</h1>
+    </header>
+  );
+}
+
+export default Header;
